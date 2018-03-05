d002b3318ceed4ea9c554eea3972a6b886c3706a
Guido Cuesta <37083926+GuidoCuesta@users.noreply.github.com>
2018-03-05 17:37:26 -0300

    Update quickSort.cpp

--- Interview Practice/Sorting/quickSort.cpp
+++ Interview Practice/Sorting/quickSort.cpp
@@ -1,3 +1,14 @@
+/*
+
+You are given an array of integers. Sort its sub-array between the given indices l and r (inclusive) and leave the other elements intact.
+
+Example
+
+For a = [5, 2, 1, 7, 5, 3, 2, 3], l = 0 and r = 3, the output should be
+quickSort(a, l, r) = [1, 2, 5, 7, 5, 3, 2, 3].
+
+*/
+
 void quickSortWithReference(std::vector<int>& a, int l, int r) {
   if (l >= r) {
     return;
