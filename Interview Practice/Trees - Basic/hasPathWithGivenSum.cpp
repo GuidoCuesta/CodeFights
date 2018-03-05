c6d2624d62b69cf1c849a6fdbae06a2a0ffbc204
GuidoCuesta <37083926+GuidoCuesta@users.noreply.github.com>
2018-03-05 16:42:13 -0300

    Create hasPathWithGivenSum.cpp

---
+++ Interview Practice/Trees - Basic/hasPathWithGivenSum.cpp
@@ -0,0 +1,179 @@
+/*
+
+Given a binary tree t and an integer s, determine whether there is a root to leaf path in t such that the sum of vertex values equals s.
+
+Example
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -2,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be hasPathWithGivenSum(t, s) = true.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -2 -3
+Path 4 -> 3 -> 2 -> -2 gives us 7, the required sum.
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -4,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be hasPathWithGivenSum(t, s) = false.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -4 -3
+There is no path from root to leaf with the given sum 7.
+
+Input/Output
+
+[execution time limit] 0.5 seconds (cpp)
+
+[input] tree.integer t
+
+A binary tree of integers.
+
+Guaranteed constraints:
+0 ≤ tree size ≤ 5 · 104,
+-1000 ≤ node value ≤ 1000.
+
+[input] integer s
+
+An integer.
+
+Guaranteed constraints:
+-4000 ≤ s ≤ 4000.
+
+[output] boolean
+
+Return true if there is a path from root to leaf in t such that the sum of node values in it is equal to s, otherwise return false.
+
+*/
+
+
+//
+// Definition for binary tree:
+// template<typename T>
+// struct Tree {
+//   Tree(const T &v) : value(v), left(nullptr), right(nullptr) {}
+//   T value;
+//   Tree *left;
+//   Tree *right;
+// };
+bool hasPathWithGivenSum(Tree<int> * t, int s) 
+{
+
+    bool retval=false;
+    int subnum=0;
+    
+    if(t==nullptr)
+        if(s==0)
+            retval=true;
+        else
+            retval=false;
+    else
+    {
+        subnum=s-t->value;
+  
+        if(subnum==0 && (t->left==nullptr && t->right==nullptr))
+            retval=true;
+        else if(t->left!=nullptr || t->right!=nullptr)
+        {
+            if (retval==false && t->left!=nullptr)
+                retval=hasPathWithGivenSum(t->left,subnum);
+  
+            if (retval==false && t->right!=nullptr)
+                retval=hasPathWithGivenSum(t->right,subnum);
+  
+        }          
+    }
+        
+    return retval;
+}
